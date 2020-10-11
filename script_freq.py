a3a7dbaac7d73620d0eaafec45eec6bc845d600f
Vinay Jain <43477666+shacvin@users.noreply.github.com>
2020-10-11 13:03:39 +0530

    Added frequency datasets
    Added frequency for word_tag and most common words and tags.

---
+++ script_freq.py
@@ -0,0 +1,16 @@
+import csv
+
+di = dict()
+with open('dataset.csv') as f1:
+    reader = csv.reader(f1)
+    for row in reader:
+      if row[0] in di:
+         di[row[0]] = di[row[0]] + 1
+      else:
+         di[row[0]] = 1
+
+with open('word_tag_frequency.csv', 'w') as f2:
+    writer = csv.writer(f2)
+    for key, value in di.items():
+            new_row = [key, value]
+            writer.writerow(new_row)            
