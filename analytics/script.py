e24591789c8a21651f879cd78c81f520f26a136e
Amish Garg <agarg@cs.iitr.ac.in>
2020-12-18 19:10:52 +0530

    remove obsolete files

--- analytics/script.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-with open('token-tag_pairs_cleaned.csv') as csv_file:
-    reader = csv.reader(csv_file)
-    with open('../dataset/naive_bayes/dataset.csv', 'w') as g:
-        writer = csv.writer(g)
-        for row in reader:
-            new_row = ['_'.join([row[0], row[1]])]
-            writer.writerow(new_row)
